b2d30097dc4d7cf8308c45bb939677a8d3c9cc9a
Sandro <sandro.luck@gmx.ch>
2015-10-26 15:33:00 +0100

    first working staff.

--- src/DataSetImporter.java
+++ src/DataSetImporter.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 //created and maintained by Sandro Luck
 //13-927-769
@@ -8,6 +9,9 @@ public class DataSetImporter {
 	private final String inputFileName;
 	private FileReader fileReader;
 	private BufferedReader bufferedReader;
+	private String [] tokens;
+	
+	private String lastRaw;
 	
 	public DataSetImporter(String inputFileName){
 		this.inputFileName=inputFileName;
@@ -18,5 +22,18 @@ public class DataSetImporter {
 			e.printStackTrace();
 		}
 		this.bufferedReader = new BufferedReader(fileReader);
+		try {
+			lastRaw=bufferedReader.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String [] tokens = lastRaw.split("\\s+");
+	    
+		String var_1 = tokens[0];
+	    String var_2 = tokens[1];
+	    String var_3 = tokens[2];
+	    
+	    System.out.println(tokens[0]+"   ELLLA   "+tokens[1]+tokens[2]);
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 //created and maintained by Sandro Luck
 //13-927-769
-//TEST MAIN
+//TEST MAIN, 3h so far,
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new DataSetImporter("movies_80000.tsv");
 	}
 
 }
