d52ec28131d45a2368b8f923a381865c0ab0a1e5
Sandro <sandro.luck@gmx.ch>
2015-10-26 15:56:30 +0100

    so test

--- src/DataSetImporter.java
+++ src/DataSetImporter.java
@@ -10,7 +10,6 @@ public class DataSetImporter {
 	private FileReader fileReader;
 	private BufferedReader bufferedReader;
 	private String [] tokens;
-	
 	private String lastRaw;
 	
 	public DataSetImporter(String inputFileName){
@@ -28,12 +27,15 @@ public class DataSetImporter {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		String [] tokens = lastRaw.split("\\s+");
+		String [] tokens = lastRaw.split("\t");
 	    
 		String var_1 = tokens[0];
 	    String var_2 = tokens[1];
 	    String var_3 = tokens[2];
+	    String var_4 = tokens[3];
+	    String var_5 = tokens[4];
+	    String var_6 = tokens[5];
 	    
-	    System.out.println(tokens[0]+"   ELLLA   "+tokens[1]+tokens[2]);
+	    System.out.println(tokens[0]+" "+tokens[1]+" "+tokens[2]+" "+tokens[3]+" "+tokens[4]+" "+tokens[5]+" "+tokens[6]);
 	}
 }
