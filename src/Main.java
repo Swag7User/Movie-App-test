b0f9462200484012a1ec7b61b9bf9aeb0318e3c5
Sandro <sandro.luck@gmx.ch>
2015-10-26 14:44:34 +0100

    Sandro basic staff

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//created and maintained by Sandro Luck
+//13-927-769
+//TEST MAIN
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
