99eaaff64bfb55ae95e93c40929109b48dbcfc1d
Swag7User <oertle.d@gmail.com>
2015-10-26 16:00:29 +0100

    fix shit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,12 +4,12 @@ public class HelloWorld {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Eat my shorts!");
     }
-
 }
 
-<<<<<<< HEAD
+
+//<<<<<< HEAD
 // EAT MY SHIT
 //mjdnmjmskv
-=======
+//=======
 // EAT MY SHIT
->>>>>>> refs/remotes/origin/Sandro
+//>>>>>>> refs/remotes/origin/Sandro
