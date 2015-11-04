fd8f34c12932180c783b4e75834f76e93c95e6a9
tarkin67 <kingoftheroads96@gmail.com>
2015-11-04 14:33:52 +0100

    modified dataSetImporter to write a sql database

--- src/DataSetImporter.java
+++ src/DataSetImporter.java
@@ -2,40 +2,74 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 //created and maintained by Sandro Luck
 //13-927-769
 public class DataSetImporter {
 	private final String inputFileName;
+	private String outputFileName;
 	private FileReader fileReader;
 	private BufferedReader bufferedReader;
 	private String [] tokens;
 	private String lastRaw;
+	private PrintWriter writer;
 	
-	public DataSetImporter(String inputFileName){
+	private boolean stop = false;
+	
+	public DataSetImporter(String inputFileName, String outputFileName){
 		this.inputFileName=inputFileName;
+		this.outputFileName = outputFileName;
 		try {
 			this.fileReader = new FileReader(inputFileName);
+			this.writer = new PrintWriter(outputFileName);
+			initializeDatabase();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		this.bufferedReader = new BufferedReader(fileReader);
 		try {
-			lastRaw=bufferedReader.readLine();
+			while(stop == false){
+				lastRaw=bufferedReader.readLine();
+				if(lastRaw == null){
+					stop = true;
+				}else{
+					String [] tokens = lastRaw.split("\t");
+				    
+				    printArrayToFile(tokens);
+				}
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		String [] tokens = lastRaw.split("\t");
-	    
-		String var_1 = tokens[0];
-	    String var_2 = tokens[1];
-	    String var_3 = tokens[2];
-	    String var_4 = tokens[3];
-	    String var_5 = tokens[4];
-	    String var_6 = tokens[5];
-	    
-	    System.out.println(tokens[0]+" "+tokens[1]+" "+tokens[2]+" "+tokens[3]+" "+tokens[4]+" "+tokens[5]+" "+tokens[6]);
+		
+	    writer.close();
+	}
+	
+	private void initializeDatabase(){
+		writer.println("CREATE DATABASE movies");
+		writer.println("USE movies");
+		writer.println("\nCREATE TABLE table");
+		writer.println("(");
+		writer.println("[wiki_id] int(11) NOT NULL AUTO_INCREMENT,");
+		writer.println("[freebase_id] char(20),");
+		writer.println("[name] char(35),");
+		writer.println("[release_date] date(),");
+		writer.println("[box_office] int(),");
+		writer.println("[runtime] int(),");
+		writer.println("[languages] char(),");
+		writer.println("[countries] char(),");
+		writer.println("[genres] char(),");
+		writer.println(")");
+	}
+	
+	private void printArrayToFile(String[] tokens){
+		writer.print("INSERT INTO table VALUES (");
+	    for(int i=0; i<tokens.length-1; i++){
+	    	writer.print("\'" + tokens[i] + "\', ");
+	    }
+	    writer.print(tokens[tokens.length-1] + "\')\n" );
 	}
 }
